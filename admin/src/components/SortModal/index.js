0952c37ea5d249f88add6d14e71ec54b9d011af4
derschiw <aeneas.meier@protonmail.com>
2022-09-22 17:10:40 +0200

    autodetect if content-type has needed fields

--- admin/src/components/SortModal/index.js
+++ admin/src/components/SortModal/index.js
@@ -7,38 +7,50 @@ import { SimpleMenu, MenuItem } from '@strapi/design-system/SimpleMenu';
 
 const SortModal = () => {
 
+  const [active, setActive] = useState(false);
   const [foo, setFoo] = useState([]);
   const [status, setStatus] = useState('loading');
 
-  
-  
+  const initializeFoo = async () => {
+    try {
+      const { data } = await axiosInstance.get(
+        `/content-manager/collection-types/${contentType}?sort=rank:asc`
+      );
+      if (!!toString(data.results[0].rank) && !!data.results[0].Bar) {
+        setActive(true);
+      }
+    } catch (e) {
+      console.log(e);
+      setStatus('error');
+    }
+  }
+
   // Fetch data from the database via get request
   const fetchFoo = async () => {
     try {
       const { data } = await axiosInstance.get(
         `/content-manager/collection-types/${contentType}?sort=rank:asc`
-        );
-        
-        setStatus('success');
-        // Iterate over all results and append them to the list
-        let list = [];
-        for (let i = 0; i < data.results.length ; i++){
-          list.push({
-            content: (<MenuItem>{data.results[i].Bar}</MenuItem>), strapiId: data.results[i].id
-          });
-        }
-        setFoo(list);
-      } catch (e) {
-        console.log(e);
-        setStatus('error');
+      );
+      setStatus('success');
+      // Iterate over all results and append them to the list
+      let list = [];
+      for (let i = 0; i < data.results.length; i++) {
+        list.push({
+          content: (<MenuItem>{data.results[i].Bar}</MenuItem>), strapiId: data.results[i].id
+        });
       }
+      setFoo(list);
+    } catch (e) {
+      console.log(e);
+      setStatus('error');
+    }
   };
 
   //
   const updateFoo = async (sortedList) => {
     try {
       // Iterate over all results and append them to the list
-      for (let i = 0; i < sortedList.length ; i++){
+      for (let i = 0; i < sortedList.length; i++) {
         await axiosInstance.put(`/drag-drop-content-types/sort-update/${sortedList[i].strapiId}`, {
           contentType: contentType,
           rank: sortedList[i].rank,
@@ -51,8 +63,23 @@ const SortModal = () => {
     }
   };
 
+  const showMenu = () => {
+    if (!active){
+      return
+    }
+    return (
+      <>
+        <SimpleMenu
+          label={Label}
+          onClick={() => { fetchFoo() }}
+        >
+          <DragSortableList items={foo} moveTransitionDuration={0.3} onSort={updateFoo} type="vertical" />
+        </SimpleMenu>
+      </>
+    )
+  }
 
-  
+
   var listHorizontal = [
     { content: (<div style={{ color: 'white' }}>test1</div>), classes: ['bigger'] },
     { content: (<div style={{ color: 'white' }}>test2</div>) },
@@ -68,19 +95,16 @@ const SortModal = () => {
   ];
 
   // Get content type from url
-  const paths       = window.location.pathname.split('/')
+  const paths = window.location.pathname.split('/')
   const contentType = paths[paths.length - 1]
   // Order menu label
-  const Label        = <>Order</>;
-  
+  const Label = <>Order</>;
+
+  initializeFoo();
+
   return (
     <>
-      <SimpleMenu 
-      label={Label}
-      onClick={() => {fetchFoo()}}
-      >
-        <DragSortableList items={foo} moveTransitionDuration={0.3} onSort={updateFoo} type="vertical" />
-      </SimpleMenu>
+      {showMenu()}
     </>
   );
 };
