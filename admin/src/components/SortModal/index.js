cf68ea0933c625cae0bdd394ed1d237c3115750d
derschiw <aeneas.meier@protonmail.com>
2022-09-23 09:10:07 +0200

    increase effciency by only updating changed values

--- admin/src/components/SortModal/index.js
+++ admin/src/components/SortModal/index.js
@@ -16,6 +16,8 @@ const SortModal = () => {
   const [foo, setFoo] = useState([]);
   const [status, setStatus] = useState('loading');
 
+  // Check database if the desired fields are available
+  // TODO: check field integrity 
   const initializeFoo = async () => {
     try {
       const { data } = await axiosInstance.get(
@@ -50,17 +52,25 @@ const SortModal = () => {
       setStatus('error');
     }
   };
-
-  //
+  
+  // Update all ranks via put request.
   const updateFoo = async (sortedList) => {
     try {
       // Iterate over all results and append them to the list
       for (let i = 0; i < sortedList.length; i++) {
-        await axiosInstance.put(`/drag-drop-content-types/sort-update/${sortedList[i].strapiId}`, {
-          contentType: contentType,
-          rank: sortedList[i].rank,
-        });
+        // Only update changed values
+        if (previousSortedList[i] && previousSortedList[i].strapiId != sortedList[i].strapiId){
+          await axiosInstance.put(`/drag-drop-content-types/sort-update/${sortedList[i].strapiId}`, {
+            contentType: contentType,
+            rank: sortedList[i].rank,
+          });
+        }
       }
+      // Store the state of the list to increase update performance
+      // TODO: Currently on the first event all entries are updated, 
+      //       since there is no previous list available. 
+      //       Get an initial state when first loading page.
+      previousSortedList = sortedList;
       setStatus('success');
     } catch (e) {
       console.log(e);
@@ -68,6 +78,7 @@ const SortModal = () => {
     }
   };
 
+  // Render the menu
   const showMenu = () => {
     if (!active) {
       return
@@ -103,6 +114,8 @@ const SortModal = () => {
   // Get content type from url
   const paths = window.location.pathname.split('/')
   const contentType = paths[paths.length - 1]
+  // Store the drag and drop lists previous value to increase update performance
+  let previousSortedList = []
 
   initializeFoo();
 
