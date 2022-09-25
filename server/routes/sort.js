e542f6446bc1321cb894d5bfe76788ba98ff5c66
derschiw <aeneas.meier@protonmail.com>
2022-09-25 13:57:35 +0200

    remove unused paths

--- server/routes/sort.js
+++ server/routes/sort.js
@@ -22,36 +22,9 @@ module.exports = {
       },
     },
     {
-      method: 'GET',
-      path: '/sort-index',
-      handler: 'sort.index',
-      config: {
-        policies: [],
-        auth: false,
-      },
-    },
-    {
-      method: 'GET',
-      path: '/count',
-      handler: 'sort.count',
-      config: {
-        policies: [],
-        auth: false,
-      },
-    },
-    {
       method: "PUT",
       path: "/sort-update/:id",
       handler: "sort.update",
-      config: {
-        policies: [],
-        auth: false,
-      },
-    },
-    {
-      method: "POST",
-      path: "/sort-create",
-      handler: "sort.create",
       config: {
         policies: [],
         auth: false,
