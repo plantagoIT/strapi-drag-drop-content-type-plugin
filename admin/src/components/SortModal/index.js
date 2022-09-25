a088833ffdaca6bdb88bf714cc1e73c45e9a98cf
derschiw <aeneas.meier@protonmail.com>
2022-09-25 10:36:07 +0200

    consider settings

--- admin/src/components/SortModal/index.js
+++ admin/src/components/SortModal/index.js
@@ -13,13 +13,13 @@ import Layer from '@strapi/icons/Layer';
 const SortModal = () => {
 
   const [active, setActive] = useState(false);
-  const [foo, setFoo] = useState([]);
+  const [contentType, setContentType] = useState([]);
   const [status, setStatus] = useState('loading');
-  const [settings, setSettings] = useState('loading');
+  const [settings, setSettings] = useState();
 
   // Shorten string to prevent line break
   const shortenString = (string) => {
-    if (string.length > 40){
+    if (string.length > 40) {
       return string.substring(0, 37) + "..."
     }
     return string
@@ -30,7 +30,6 @@ const SortModal = () => {
     try {
       const { data } = await axiosInstance.get(`drag-drop-content-types/settings`);
       setSettings(data.body);
-      console.log(settings);
     } catch (e) {
       console.log(e);
     }
@@ -38,12 +37,12 @@ const SortModal = () => {
 
   // Check database if the desired fields are available
   // TODO: check field integrity 
-  const initializeFoo = async () => {
+  const initializeContentType = async () => {
     try {
       const { data } = await axiosInstance.get(
-        `/content-manager/collection-types/${contentType}?sort=rank:asc`
+        `/content-manager/collection-types/${contentTypePath}?sort=rank:asc`
       );
-      if (!!toString(data.results[0].rank) && !!data.results[0].Bar) {
+      if (!!toString(data.results[0][settings.rank]) && !!data.results[0][settings.title]) {
         setActive(true);
       }
     } catch (e) {
@@ -53,36 +52,36 @@ const SortModal = () => {
   }
 
   // Fetch data from the database via get request
-  const fetchFoo = async () => {
+  const fetchContentType = async () => {
     try {
       const { data } = await axiosInstance.get(
-        `/content-manager/collection-types/${contentType}?sort=rank:asc`
+        `/content-manager/collection-types/${contentTypePath}?sort=rank:asc`
       );
       setStatus('success');
       // Iterate over all results and append them to the list
       let list = [];
       for (let i = 0; i < data.results.length; i++) {
         list.push({
-          content: (<MenuItem ><Icon height={"0.6rem"} as={Drag} />&nbsp;<span title={data.results[i].Bar}>{shortenString(data.results[i].Bar)}</span></MenuItem>), strapiId: data.results[i].id
+          content: (<MenuItem ><Icon height={"0.6rem"} as={Drag} />&nbsp;<span title={data.results[i][settings.title]}>{shortenString(data.results[i][settings.title])}</span></MenuItem>), strapiId: data.results[i].id
         });
       }
-      setFoo(list);
+      setContentType(list);
     } catch (e) {
       console.log(e);
       setStatus('error');
     }
   };
-  
+
   // Update all ranks via put request.
-  const updateFoo = async (sortedList) => {
+  const updateContentType = async (sortedList) => {
     try {
       // Iterate over all results and append them to the list
       for (let i = 0; i < sortedList.length; i++) {
         // Only update changed values
-        if (previousSortedList[i] && previousSortedList[i].strapiId != sortedList[i].strapiId){
+        if (previousSortedList[i] && previousSortedList[i].strapiId != sortedList[i].strapiId) {
           // Update rank via put request
           await axiosInstance.put(`/drag-drop-content-types/sort-update/${sortedList[i].strapiId}`, {
-            contentType: contentType,
+            contentType: contentTypePath,
             rank: sortedList[i].rank,
           });
         }
@@ -109,39 +108,30 @@ const SortModal = () => {
         <SimpleMenu
           as={IconButton}
           icon={<Layer />}
-          onClick={() => { fetchFoo() }}
+          onClick={() => { fetchContentType() }}
         >
-          <DragSortableList items={foo} moveTransitionDuration={0.3} onSort={updateFoo} type="vertical" />
+          <DragSortableList items={contentType} moveTransitionDuration={0.3} onSort={updateContentType} type="vertical" />
         </SimpleMenu>
       </>
     )
   }
 
+  // Fetch settings on page render
+  useEffect(() => {
+    fetchSettings();
+  }, [])
 
-  var listHorizontal = [
-    { content: (<div style={{ color: 'white' }}>test1</div>), classes: ['bigger'] },
-    { content: (<div style={{ color: 'white' }}>test2</div>) },
-    { content: (<div style={{ color: 'white' }}>test3</div>), classes: ['bigger'] },
-    { content: (<div style={{ color: 'white' }}>test4</div>) }
-  ];
-
-  var listGrid = [
-    { content: (<div style={{ color: 'white' }}>test1</div>) },
-    { content: (<div style={{ color: 'white' }}>test2</div>) },
-    { content: (<div style={{ color: 'white' }}>test3</div>) },
-    { content: (<div style={{ color: 'white' }}>test4</div>) },
-  ];
+  // Update view when settings change
+  useEffect(() => {
+    initializeContentType();
+  }, [settings])
 
   // Get content type from url
   const paths = window.location.pathname.split('/')
-  const contentType = paths[paths.length - 1]
+  const contentTypePath = paths[paths.length - 1]
+
   // Store the drag and drop lists previous value to increase update performance
   let previousSortedList = []
-
-  useEffect(() => {
-    fetchSettings();
-    initializeFoo();
-  }, [])
 
   return (
     <>
