cd3209dea340ec67f121afd79b63458ee619876b
derschiw <aeneas.meier@protonmail.com>
2022-10-11 11:14:44 +0200

    remove raboo column

--- admin/src/index.js
+++ admin/src/index.js
@@ -44,26 +44,6 @@ export default {
       name: "sort-component",
       Component: SortModal,
     });
-
-    app.registerHook('Admin/CM/pages/ListView/inject-column-in-table', ({ displayedHeaders, layout }) => {
-      return {
-        layout,
-        displayedHeaders: [
-          ...displayedHeaders,
-          {
-            key: '__locale_key__', // Needed for the table
-            fieldSchema: { type: 'string' }, // Schema of the attribute
-            metadatas: {
-              label: 'Raboo', // Label of the header,
-              sortable: true | false // Define if the column is sortable
-            }, // Metadatas for the label
-            // Name of the key in the data we will display
-            name: 'locales',
-            // Custom renderer: props => Object.keys(props).map(key => <p key={key}>key</p>)
-          },
-        ]
-      }
-    });
   },
 
   async registerTrads({ locales }) {
